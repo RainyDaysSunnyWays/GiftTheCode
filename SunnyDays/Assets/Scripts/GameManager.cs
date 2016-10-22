8c16ef3637ee0013470276944b35ed9a40bbb265
Sabet <masabet@deloitte.ca>
2016-10-22 00:33:58 -0400

    Create Level 1 skeleton

---
+++ SunnyDays/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 1f;                  //How long to wait before starting each new level
+    public Text verbText;
+
+	// Use this for initialization
+	void Start () {
+        verbText.text = "Push!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerable NextLevel()
+    {
+        yield return new WaitForSeconds(levelStartDelay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);					//Load the next scene
+    }
+}
