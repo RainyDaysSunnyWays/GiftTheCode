7f059f353f3134fe0e41cb3508d63597c4676965
Mahtab Sabet <mahtabz@gmail.com>
2016-10-22 14:30:34 -0400

    Finished high five level

---
+++ SunnyDays/Assets/Scripts/SealController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SealController : MonoBehaviour {
+
+
+    public Sprite idleSeal;
+    public Sprite happySeal;
+    public Sprite sadSeal;
+
+    private SpriteRenderer spriteRenderer;
+
+    public HandController handController;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = idleSeal;
+    }
+	
+	public IEnumerator MakeSealHappy()
+    {
+        spriteRenderer.sprite = happySeal;
+        yield return new WaitForSeconds(handController.highFiveDuration);
+        MakeSealIdle();
+    }
+
+    public IEnumerator MakeSealSad()
+    {
+        spriteRenderer.sprite = sadSeal;
+        yield return new WaitForSeconds(handController.highFiveDuration);
+        MakeSealIdle();
+    }
+
+    public void MakeSealIdle()
+    {
+        spriteRenderer.sprite = idleSeal;
+    }
+}
