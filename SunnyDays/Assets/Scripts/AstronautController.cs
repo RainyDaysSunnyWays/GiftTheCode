5fabb175e00fed5a6630be5a37a1e49561fa5d9c
Meep <mwraich@financeit.io>
2016-10-22 23:41:32 -0400

    set up astronaut level basics

---
+++ SunnyDays/Assets/Scripts/AstronautController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstronautController : MonoBehaviour {
+	[HideInInspector] public bool jump = true;
+
+	public float jumpForce = 600;
+	public Transform groundCheck;
+	
+
+	private bool grounded = false;
+	private Rigidbody2D rb2d;
+
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+		
+	void Update () {
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+		if (Input.GetButtonDown ("Jump") && grounded) {
+			jump = true;
+		}
+	}
+
+	void FixedUpdate(){
+		if (jump) {
+			rb2d.AddForce (new Vector2 (0f, jumpForce));
+			jump = false;
+		}
+	}
+}
---
+++ SunnyDays/Assets/Scripts/StarsController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarsController : MonoBehaviour {
+	public float rotationSpeed = 5.0f;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	private Vector3 zAxis = new Vector3(0, 0, 1);
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.RotateAround(target.position, zAxis, rotationSpeed); 
+	}
+}
